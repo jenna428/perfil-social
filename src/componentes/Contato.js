dfee604c9ed4e85e3b650d31bb2f28ec5d2fcf84
Jeniffer Lorhaynne <162042280+jenna428@users.noreply.github.com>
2024-09-07 20:17:38 -0300

    links inseridos

--- src/componentes/Contato.js
+++ src/componentes/Contato.js
@@ -6,10 +6,10 @@ function Contato(){
     return(
         <div className={styles.redes}>
             <ul className={styles.list}>
-                <li><FaInstagram/></li>
-                <li><FaLinkedin/></li>
-                <li><FaGithub/></li>
-                <li><CgMail/></li>
+                <li><a href='https://www.instagram.com/jenny_lorhaynne/' target='_blank'><FaInstagram/></a></li>
+                <li><a href='https://www.linkedin.com/in/jeniffer-lorhaynne-nascimento-96a42b2a1' target='_blank'><FaLinkedin/></a></li>
+                <li><a href='https://github.com/jenna428' target='_blank'><FaGithub/></a></li>
+                <li><a href='mailto:jeniffer28nascimento@gmail.com'><CgMail/></a></li>
             </ul>
         </div>
     )
